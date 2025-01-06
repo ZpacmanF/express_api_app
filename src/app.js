fb319e4432f3b363a033967a4bb912ac5013ceb9
ZpacmanF <joaocarvalho.2002@alunos.utfpr.edu.br>
2025-01-06 15:39:21 -0300

    feat: implement security rules in app.js

--- src/app.js
+++ src/app.js
@@ -1,13 +1,32 @@
 const express = require('express');
+const helmet = require('helmet');
+const compression = require('compression');
+const cors = require('cors');
+const morgan = require('morgan');
 const connectDB = require('./config/database');
+
+
+const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
+const productRoutes = require('./routes/productRoutes');
 
 const app = express();
 
 connectDB();
 
-app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(compression());
+app.use(morgan('combined'));
+app.use(express.json({ limit: '10kb' }));
 
+app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/products', productRoutes);
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ message: 'Something went wrong!' });
+});
 
 module.exports = app;
