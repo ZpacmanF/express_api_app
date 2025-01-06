e19d92f2e93baa870c4c2dcafd3ab3c2b27abc0d
ZpacmanF <joaocarvalho.2002@alunos.utfpr.edu.br>
2025-01-06 15:39:43 -0300

    fix: sanitize user routes

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const { protect, checkPermission } = require('../middleware/authMiddleware');
+const { check } = require('express-validator');
 
-router.post('/', userController.createUser);
+const userValidation = [
+    check('name').trim().notEmpty(),
+    check('email').isEmail().normalizeEmail(),
+    check('password').isLength({ min: 6 })
+];
+
+router.post('/', userValidation, userController.createUser);
+
+router.use(protect);
 
 router.get('/', userController.getAllUsers);
+
 router.get('/:id', userController.getUserById);
-router.put('/:id', userController.updateUser);
-router.delete('/:id', userController.deleteUser);
+
+router.put('/:id', userValidation, checkPermission, userController.updateUser);
+
+router.delete('/:id', checkPermission, userController.deleteUser);
 
 module.exports = router;
