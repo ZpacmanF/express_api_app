fbcb5f1bc35bb1560fd14d6fe1b0cdf6c326fd71
ZpacmanF <joaocarvalho.2002@alunos.utfpr.edu.br>
2025-01-07 14:05:53 -0300

    fix: adjust password validation in userModel

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -45,6 +45,7 @@ userSchema.pre('save', async function(next) {
 });
 
 userSchema.methods.matchPassword = async function(enteredPassword) {
+    if (!this.password) return false;
     return await bcrypt.compare(enteredPassword, this.password);
 };
 
