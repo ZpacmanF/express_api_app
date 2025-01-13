9860b172932e3984fee7e19484e69f3fe83deaad
ZpacmanF <joaocarvalho.2002@alunos.utfpr.edu.br>
2025-01-13 11:03:14 -0300

    chore: move logs to temporary folders for better management

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,12 +1,14 @@
 const winston = require('winston');
 const path = require('path');
 
+const logDirectory = path.join('/tmp', 'logs');
+
 const logger = winston.createLogger({
   level: 'info',
   format: winston.format.json(),
   transports: [
-    new winston.transports.File({ filename: path.join('logs', 'error.log'), level: 'error' }),
-    new winston.transports.File({ filename: path.join('logs', 'combined.log') })
+    new winston.transports.File({ filename: path.join(logDirectory, 'error.log'), level: 'error' }),
+    new winston.transports.File({ filename: path.join(logDirectory, 'combined.log') })
   ]
 });
 
